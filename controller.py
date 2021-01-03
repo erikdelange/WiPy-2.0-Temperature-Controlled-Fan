ee0f069ad97c15d0d53d47d7cf9a499e9e45bbe6
Erik de Lange <erikdelange@users.noreply.github.com>
2021-01-03 17:19:47 +0100

    Add files via upload

--- controller.py
+++ controller.py
@@ -21,10 +21,15 @@ def nvs_get(key, default_value=0):
     :param int default_value: value to use when creating the key
     :return value int: (NVRAM can only store integers)
     """
-    value = pycom.nvs_get(key)
+    try:
+        value = pycom.nvs_get(key)
+    except ValueError:  # certain firmware versions throw an (undocumented) ValueError
+        value = None
+
     if value is None:
         pycom.nvs_set(key, int(default_value))
         value = int(default_value)
+
     return value
 
 
@@ -32,20 +37,20 @@ pwm = PWM(0, frequency=25000)
 channel0 = pwm.channel(0, pin="P23", duty_cycle=1.0)
 channel1 = pwm.channel(0, pin="P22", duty_cycle=1.0)
 
-lock = _thread.allocate_lock()
-
 temp0 = 0
 temp1 = 0
 
-dutycycle = 0  # unit %, where 0 if off and 100 is full speed
+dutycycle = 0  # unit = %, where 0 if off and 100 is full speed
 
-max_dutycycle = nvs_get("max_dutycycle", 100)  # unit = %
-min_dutycycle = nvs_get("min_dutycycle", 20)  # unit = %
+start_dutycycle = nvs_get("start_dutycycle", 100)  # duty cycle at temp_fan_on, unit = %
+end_dutycycle = nvs_get("end_dutycycle", 20)  # duty cycle at temp_fan_end, unit = %
 
 temp_fan_on = nvs_get("temp_fan_on", 40)  # unit = degrees celsius
-temp_fan_max = nvs_get("temp_fan_max", 60)  # unit = degrees celsius
+temp_fan_end = nvs_get("temp_fan_end", 60)  # unit = degrees celsius
 
 hysteresis = nvs_get("hysteresis", 2)  # unit = degrees celsius
+
+lock = _thread.allocate_lock()
 
 
 def set_dutycycle(d):
@@ -89,108 +94,136 @@ def daemon():
 
         if temp < (temp_fan_on - hysteresis):
             dutycycle = 0
-            set_dutycycle(dutycycle)
 
-        if temp > temp_fan_on:
-            dutycycle_range = max_dutycycle - min_dutycycle
-            temp_range= temp_fan_max - temp_fan_on
-            temp_clipped = min(max(temp - temp_fan_on, 0), temp_range)
-            dutycycle_raw = int(temp_clipped * (dutycycle_range / temp_range))
-            dutycycle = 0 if dutycycle_raw <= 0 else min_dutycycle + dutycycle_raw
-            set_dutycycle(dutycycle)
+        if temp >= temp_fan_on:
+            dutycycle_range = end_dutycycle - start_dutycycle
+
+            if dutycycle_range == 0:  # every temp has same duty cycle
+                dutycycle = start_dutycycle
+            else:
+                temp_range = temp_fan_end - temp_fan_on
+
+                if temp_range == 0:  # from nought to 100% in one step
+                    dutycycle = end_dutycycle
+                else:
+                    temp_clipped = min(max(temp - temp_fan_on, 0), temp_range)
+                    dutycycle_raw = int(temp_clipped * (dutycycle_range / temp_range))
+                    dutycycle = start_dutycycle + dutycycle_raw
+
+        set_dutycycle(dutycycle)
 
 
 _thread.start_new_thread(daemon, ())
 
-try:
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind(socket.getaddrinfo("0.0.0.0", 80)[0][-1])
-    serversocket.listen()
 
-    while True:
-        gc.collect()
-        # print(gc.mem_free())
+def server():
+    """ Web interface. """
+    global temp0, temp1, dutycycle
+    global start_dutycycle, end_dutycycle, temp_fan_on, temp_fan_end, hysteresis
 
-        conn, addr = serversocket.accept()
-        request_line = conn.readline()
+    try:
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except Exception as e:
+        raise(e)
 
-        # print("request:", request_line, "from", addr)
-
-        if request_line in [b"", b"\r\n"]:
-            # print("malformed request")
-            conn.close()
-            continue
-
-        request = url.request(request_line)
-        header = request["header"]
+    try:
+        serversocket.bind(socket.getaddrinfo("0.0.0.0", 80)[0][-1])
+        serversocket.listen()
 
         while True:
-            line = conn.readline()
-            if line in [b"", b"\r\n"]:
+            gc.collect()   # for devices with limited memory (WiPy 2)
+            print(gc.mem_free() / 1024, "Kb free")
+
+            conn, addr = serversocket.accept()
+            request_line = conn.readline()
+
+            print("request:", request_line, "from", addr)
+
+            if request_line in [b"", b"\r\n"]:
+                print("malformed request")
+                conn.close()
+                continue
+
+            request = url.request(request_line)
+            header = request["header"]
+
+            while True:
+                line = conn.readline()
+                if line in [b"", b"\r\n"]:
+                    break
+
+                # add header fields to dictionary 'header'
+                semicolon = line.find(b":")
+                if semicolon != -1:
+                    key = line[0:semicolon].decode("utf-8")
+                    value = line[semicolon+1:-2].lstrip().decode("utf-8")
+                    header[key] = value
+
+            conn.write("HTTP/1.1 200 OK\nServer: WiPy\n")
+            conn.write("Connection: close\nContent-Type: text/html\n\n")
+
+            path = request["path"]
+
+            if path == "/":
+                http.sendfile(conn, "index.html")
+
+            if path == "/favicon.ico":
+                http.sendfile(conn, "favicon.ico")
+
+            if path == "/api/stop":  # stop server, for dev purposes
+                conn.write("\n")
+                conn.close()
                 break
 
-            # add header fields to dictionary 'header'
-            semicolon = line.find(b":")
-            if semicolon != -1:
-                key = line[0:semicolon].decode("utf-8")
-                value = line[semicolon+1:-2].lstrip().decode("utf-8")
-                header[key] = value
+            if path == "/api/reset":
+                conn.write("\n")
+                conn.close()
+                reset()
 
-        conn.write("HTTP/1.1 200 OK\nServer: WiPy\n")
-        conn.write("Connection: close\nContent-Type: text/html\n\n")
+            if path == "/api/init":
+                settings = dict()
+                with lock:
+                    settings["temp0"] = max(temp0, temp1)
+                    settings["temp1"] = min(temp0, temp1)
+                settings["dutycycle"] = dutycycle
+                settings["end_dutycycle"] = end_dutycycle
+                settings["start_dutycycle"] = start_dutycycle
+                settings["temp_fan_on"] = temp_fan_on
+                settings["temp_fan_end"] = temp_fan_end
+                settings["hysteresis"] = hysteresis
+                conn.write(json.dumps(settings))
 
-        path = request["path"]
+            if path == "/api/set":
+                parameters = request["parameters"]
+                if "end_dutycycle" in parameters:
+                    end_dutycycle = int(parameters["end_dutycycle"])
+                    pycom.nvs_set("end_dutycycle", end_dutycycle)
+                if "start_dutycycle" in parameters:
+                    start_dutycycle = int(parameters["start_dutycycle"])
+                    pycom.nvs_set("start_dutycycle", start_dutycycle)
+                if "temp_fan_on" in parameters:
+                    temp_fan_on = int(parameters["temp_fan_on"])
+                    pycom.nvs_set("temp_fan_on", temp_fan_on)
+                if "temp_fan_end" in parameters:
+                    temp_fan_end = int(parameters["temp_fan_end"])
+                    pycom.nvs_set("temp_fan_end", temp_fan_end)
+                if "hysteresis" in parameters:
+                    hysteresis = int(parameters["hysteresis"])
+                    pycom.nvs_set("hysteresis", hysteresis)
+                conn.write(json.dumps(parameters))
 
-        if path == "/":
-            http.sendfile(conn, "index.html")
+            if path == "/api/status":
+                values = dict()
+                with lock:
+                    values["temp0"] = max(temp0, temp1)
+                    values["temp1"] = min(temp0, temp1)
+                values["dutycycle"] = dutycycle
+                conn.write(json.dumps(values))
 
-        if path == "/api/reset":
             conn.write("\n")
             conn.close()
-            reset()
-
-        if path == "/api/init":
-            settings = dict()
-            with lock:
-                settings["temp0"] = max(temp0, temp1)
-                settings["temp1"] = min(temp0, temp1)
-            settings["dutycycle"] = dutycycle
-            settings["max_dutycycle"] = max_dutycycle
-            settings["min_dutycycle"] = min_dutycycle
-            settings["temp_fan_on"] = temp_fan_on
-            settings["temp_fan_max"] = temp_fan_max
-            settings["hysteresis"] = hysteresis
-            conn.write(json.dumps(settings))
-
-        if path == "/api/set":
-            parameters = request["parameters"]
-            if "max_dutycycle" in parameters:
-                max_dutycycle = int(parameters["max_dutycycle"])
-                pycom.nvs_set("max_dutycycle", max_dutycycle)
-            if "min_dutycycle" in parameters:
-                min_dutycycle = int(parameters["min_dutycycle"])
-                pycom.nvs_set("min_dutycycle", min_dutycycle)
-            if "temp_fan_on" in parameters:
-                temp_fan_on = int(parameters["temp_fan_on"])
-                pycom.nvs_set("temp_fan_on", temp_fan_on)
-            if "temp_fan_max" in parameters:
-                temp_fan_max = int(parameters["temp_fan_max"])
-                pycom.nvs_set("temp_fan_max", temp_fan_max)
-            if "hysteresis" in parameters:
-                hysteresis = int(parameters["hysteresis"])
-                pycom.nvs_set("hysteresis", hysteresis)
-            conn.write(json.dumps(parameters))
-
-        if path == "/api/status":
-            values = dict()
-            with lock:
-                values["temp0"] = max(temp0, temp1)
-                values["temp1"] = min(temp0, temp1)
-            values["dutycycle"] = dutycycle
-            conn.write(json.dumps(values))
-
-        conn.write("\n")
-        conn.close()
-except Exception as e:
-    # print("Exception", e)
-    pass
+    except Exception as e:
+        serversocket.close()
+        raise e
+    finally:
+        serversocket.close()
--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import controller
+import pycom
+
+
+pycom.heartbeat(False)
+
+while True:
+    try:
+        print("starting server")
+        pycom.rgbled(0x001000)  # green led if server is working
+        controller.server()
+        pycom.heartbeat(True)  # flashing blue led if server was stopped normally
+        break
+    except Exception as e:
+        print("server exception", repr(e))
+        pycom.rgbled(0x100000)  # red led if an error occured
+        import sys
+        sys.print_exception(e)
+        break  # dev only, to be replaced by sleep() and machine.reset()
