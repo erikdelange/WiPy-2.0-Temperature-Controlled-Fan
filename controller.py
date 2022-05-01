4c90e3cbbb685f2d659718f9fb03b0c7f2020b31
Erik de Lange <erikdelange@users.noreply.github.com>
2022-05-01 18:18:28 +0200

    Add files via upload

--- controller.py
+++ controller.py
@@ -1,229 +1,275 @@
-import gc
-import url
-import json
-import http
-import time
-import pycom
-import socket
 import _thread
+import json
+import time
 
-from machine import Pin
-from machine import PWM
-from machine import reset
-from onewire import DS18X20
-from onewire import OneWire
+import logging
+import machine
+import pycom
+import gc
+
+from httpserver import CRLF, MimeType, ResponseHeader, Server, StatusLine, sendfile
+from onewire import DS18X20, OneWire
+
+logger = logging.getLogger(__name__)
 
 
 def nvs_get(key, default_value=0):
     """ Get value for key from NVRAM. Create the key if it does not exist.
 
-    :param str key:
-    :param int default_value: value to use when creating the key
-    :return value int: (NVRAM can only store integers)
+    :param str key: get value for this key
+    :param int default_value: value to use when key must be created
+    :return int: value (note: NVRAM can only store integers)
     """
+    if len(key) > 15:
+        logger.warning("nvs_get({}): key length is over 15 characters, key truncated".format(key))
+        key = key[:15]
+
     try:
         value = pycom.nvs_get(key)
     except ValueError:  # certain firmware versions throw an (undocumented) ValueError
         value = None
-
-    if value is None:
-        pycom.nvs_set(key, int(default_value))
-        value = int(default_value)
+    finally:
+        if value is None:
+            pycom.nvs_set(key, int(default_value))
+            value = int(default_value)
 
     return value
 
 
-pwm = PWM(0, frequency=25000)
-channel0 = pwm.channel(0, pin="P23", duty_cycle=1.0)
-channel1 = pwm.channel(0, pin="P22", duty_cycle=1.0)
+def nvs_set(key, value):
+    """ Local version of pycom.nvs_set which truncates key length to 15 characters """
+    pycom.nvs_set(key[:15], value)
 
-temp0 = 0
-temp1 = 0
 
-dutycycle = 0  # unit = %, where 0 if off and 100 is full speed
+class PwmController:
 
-start_dutycycle = nvs_get("start_dutycycle", 100)  # duty cycle at temp_fan_on, unit = %
-end_dutycycle = nvs_get("end_dutycycle", 20)  # duty cycle at temp_fan_end, unit = %
+    def __init__(self):
+        pwm = machine.PWM(0, frequency=25000)
+        self.channel0 = pwm.channel(0, pin="P23", duty_cycle=1.0)
+        self.channel1 = pwm.channel(0, pin="P22", duty_cycle=1.0)
 
-temp_fan_on = nvs_get("temp_fan_on", 40)  # unit = degrees celsius
-temp_fan_end = nvs_get("temp_fan_end", 60)  # unit = degrees celsius
+        self.temp_in = 0  # inlet water temperature
+        self.temp_out = 0  # outlet water temperature
 
-hysteresis = nvs_get("hysteresis", 2)  # unit = degrees celsius
+        self.duty_cycle = 0  # unit = %, where 0 if off and 100 is full speed
+
+        self._start_duty_cycle = nvs_get("start_duty_cycle", 100)  # duty cycle at temp_fan_on, unit = %
+        self._end_duty_cycle = nvs_get("end_duty_cycle", 20)  # duty cycle at temp_fan_end, unit = %
+
+        self._temp_fan_on = nvs_get("temp_fan_on", 40)  # unit = degrees celsius
+        self._temp_fan_end = nvs_get("temp_fan_end", 60)  # unit = degrees celsius
+
+        self._hysteresis = nvs_get("hysteresis", 2)  # unit = degrees celsius
+
+    @property
+    def start_duty_cycle(self):
+        return self._start_duty_cycle
+
+    @start_duty_cycle.setter
+    def start_duty_cycle(self, value):
+        self._start_duty_cycle = value
+        nvs_set("start_duty_cycle", value)
+
+    @property
+    def end_duty_cycle(self):
+        return self._end_duty_cycle
+
+    @end_duty_cycle.setter
+    def end_duty_cycle(self, value):
+        self._end_duty_cycle = value
+        nvs_set("end_duty_cycle", value)
+
+    @property
+    def temp_fan_on(self):
+        return self._temp_fan_on
+
+    @temp_fan_on.setter
+    def temp_fan_on(self, value):
+        self._temp_fan_on = value
+        nvs_set("temp_fan_on", value)
+
+    @property
+    def temp_fan_end(self):
+        return self._temp_fan_end
+
+    @temp_fan_end.setter
+    def temp_fan_end(self, value):
+        self._temp_fan_end = value
+        nvs_set("temp_fan_end", value)
+
+    @property
+    def hysteresis(self):
+        return self._hysteresis
+
+    @hysteresis.setter
+    def hysteresis(self, value):
+        self._hysteresis = value
+        nvs_set("hysteresis", value)
+
+    def calc_duty_cycle(self, temp):
+        """ Convert temperature to corresponding duty cycle
+
+        :param int temp: temperature
+        :return int: duty cycle
+        """
+        if temp < (self.temp_fan_on - self.hysteresis):
+            self.duty_cycle = 0
+
+        if temp >= self.temp_fan_on:
+            duty_cycle_range = self.end_duty_cycle - self.start_duty_cycle
+
+            if duty_cycle_range == 0:  # every temp has same duty cycle
+                self.duty_cycle = self.start_duty_cycle
+            else:
+                temp_range = self.temp_fan_end - self.temp_fan_on
+
+                if temp_range == 0:  # from nought to 100% in one step
+                    self.duty_cycle = self.end_duty_cycle
+                else:
+                    temp_clipped = min(max(temp - self.temp_fan_on, 0), temp_range)
+                    duty_cycle_raw = int(temp_clipped * (duty_cycle_range / temp_range))
+                    self.duty_cycle = self.start_duty_cycle + duty_cycle_raw
+
+        return self.duty_cycle
+
+    def set_duty_cycle(self, duty_cycle):
+        """ Set the PWM duty cycle for the fans
+
+        :param int duty_cycle: duty cycle as percentage from 0 (stop) to 100 (full speed)
+        """
+        d = (100 - duty_cycle) / 100  # invert and scale from 0 to 1
+
+        self.channel0.duty_cycle(d)
+        self.channel1.duty_cycle(d)
+
+
+pwm = PwmController()
 
 lock = _thread.allocate_lock()
 
 
-def set_dutycycle(d):
-    """ Set the PWM dutycycle for the fans.
-
-    :param int d: dutycycle as percentage from 0 (stop) to 100 (full speed)
-    """
-    d = (100 - d) / 100  # invert and scale from 0 to 1
-
-    channel0.duty_cycle(d)
-    channel1.duty_cycle(d)
-
-
-def daemon():
-    """ Measure the temperature at fixed intervals and adjust fan speed. """
-    global temp0, temp1, dutycycle
-
-    ds = DS18X20(OneWire(Pin("P21")))
+def controller_daemon():
+    """ Measure the temperature at fixed intervals and adjust fan speed """
+    ds = DS18X20(OneWire(machine.Pin("P21")))
 
     if len(ds.roms) == 0:
-        print("no temperature sensors found")
-        _thread.exit()
+        logger.error("no temperature sensors found")
+        # _thread.exit()
+        while True:
+            gc.collect()
+            time.sleep(30)
 
-    t = 0
+    temp = [0 for _ in range(len(ds.roms))]
 
     while True:
-        # measure +- every 30 seconds
-        time.sleep(27)
+        gc.collect()
+        time.sleep(27)  # measure +- every 30 seconds
         for i in range(len(ds.roms)):
             time.sleep_ms(750)
             ds.start_conversion(ds.roms[i])
             time.sleep_ms(750)
-            t = int(ds.read_temp_async(ds.roms[i]))
-            with lock:
-                if i == 0:
-                    temp0 = t
-                else:
-                    temp1 = t
+            temp[i] = int(ds.read_temp_async(ds.roms[i]))
 
-        temp = max(temp0, temp1)  # inlet temperature is highest
+        with lock:
+            pwm.temp_in = max(temp)
+            pwm.temp_out = min(temp)
 
-        if temp < (temp_fan_on - hysteresis):
-            dutycycle = 0
-
-        if temp >= temp_fan_on:
-            dutycycle_range = end_dutycycle - start_dutycycle
-
-            if dutycycle_range == 0:  # every temp has same duty cycle
-                dutycycle = start_dutycycle
-            else:
-                temp_range = temp_fan_end - temp_fan_on
-
-                if temp_range == 0:  # from nought to 100% in one step
-                    dutycycle = end_dutycycle
-                else:
-                    temp_clipped = min(max(temp - temp_fan_on, 0), temp_range)
-                    dutycycle_raw = int(temp_clipped * (dutycycle_range / temp_range))
-                    dutycycle = start_dutycycle + dutycycle_raw
-
-        set_dutycycle(dutycycle)
+        pwm.calc_duty_cycle(pwm.temp_in)
+        pwm.set_duty_cycle(pwm.duty_cycle)
 
 
-_thread.start_new_thread(daemon, ())
+_thread.start_new_thread(controller_daemon, ())
+
+app = Server(timeout=10)
 
 
-def server():
-    """ Web interface. """
-    global temp0, temp1, dutycycle
-    global start_dutycycle, end_dutycycle, temp_fan_on, temp_fan_end, hysteresis
+@app.route("GET", "/")
+def root(conn, request):
+    conn.write(StatusLine.OK_200)
+    conn.write(ResponseHeader.CONNECTION_CLOSE)
+    conn.write(MimeType.TEXT_HTML)
+    conn.write(CRLF)
+    sendfile(conn, "index.html")
 
-    try:
-        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    except Exception as e:
-        raise(e)
 
-    try:
-        serversocket.bind(socket.getaddrinfo("0.0.0.0", 80)[0][-1])
-        serversocket.listen()
+@app.route("GET", "/favicon.ico")
+def favicon(conn, request):
+    conn.write(StatusLine.OK_200)
+    conn.write(ResponseHeader.CONNECTION_CLOSE)
+    conn.write(MimeType.IMAGE_X_ICON)
+    conn.write(CRLF)
+    sendfile(conn, "favicon.ico")
 
-        while True:
-            gc.collect()   # for devices with limited memory (WiPy 2)
-            print(gc.mem_free() / 1024, "Kb free")
 
-            conn, addr = serversocket.accept()
-            request_line = conn.readline()
+@app.route("GET", "/api/init")
+def init(conn, request):
+    settings = dict()
+    with lock:
+        settings["temp_in"] = pwm.temp_in
+        settings["temp_out"] = pwm.temp_out
+    settings["duty_cycle"] = pwm.duty_cycle
+    settings["end_duty_cycle"] = pwm.end_duty_cycle
+    settings["start_duty_cycle"] = pwm.start_duty_cycle
+    settings["temp_fan_on"] = pwm.temp_fan_on
+    settings["temp_fan_end"] = pwm.temp_fan_end
+    settings["hysteresis"] = pwm.hysteresis
+    conn.write(StatusLine.OK_200)
+    conn.write(ResponseHeader.CONNECTION_CLOSE)
+    conn.write(MimeType.APPLICATION_JSON)
+    conn.write(CRLF)
+    conn.write(json.dumps(settings))
 
-            print("request:", request_line, "from", addr)
 
-            if request_line in [b"", b"\r\n"]:
-                print("malformed request")
-                conn.close()
-                continue
+@app.route("GET", "/api/set")
+def set(conn, request):
+    parameters = request["parameters"]
+    if "start_duty_cycle" in parameters:
+        pwm.start_duty_cycle = int(parameters["start_duty_cycle"])
+    if "end_duty_cycle" in parameters:
+        pwm.end_duty_cycle = int(parameters["end_duty_cycle"])
+    if "temp_fan_on" in parameters:
+        pwm.temp_fan_on = int(parameters["temp_fan_on"])
+    if "temp_fan_end" in parameters:
+        pwm.temp_fan_end = int(parameters["temp_fan_end"])
+    if "hysteresis" in parameters:
+        pwm.hysteresis = int(parameters["hysteresis"])
+    conn.write(StatusLine.OK_200)
+    conn.write(ResponseHeader.CONNECTION_CLOSE)
+    conn.write(MimeType.APPLICATION_JSON)
+    conn.write(CRLF)
+    conn.write(json.dumps(parameters))
 
-            request = url.request(request_line)
-            header = request["header"]
 
-            while True:
-                line = conn.readline()
-                if line in [b"", b"\r\n"]:
-                    break
+@app.route("GET", "/api/status")
+def status(conn, request):
+    values = dict()
+    with lock:
+        values["temp_in"] = pwm.temp_in
+        values["temp_out"] = pwm.temp_out
+    values["duty_cycle"] = pwm.duty_cycle
+    values["mem_free"] = gc.mem_free()
+    conn.write(StatusLine.OK_200)
+    conn.write(ResponseHeader.CONNECTION_CLOSE)
+    conn.write(MimeType.APPLICATION_JSON)
+    conn.write(CRLF)
+    conn.write(json.dumps(values))
 
-                # add header fields to dictionary 'header'
-                semicolon = line.find(b":")
-                if semicolon != -1:
-                    key = line[0:semicolon].decode("utf-8")
-                    value = line[semicolon+1:-2].lstrip().decode("utf-8")
-                    header[key] = value
 
-            conn.write("HTTP/1.1 200 OK\nServer: WiPy\n")
-            conn.write("Connection: close\nContent-Type: text/html\n\n")
+@app.route("GET", "/api/stop")
+def stop(conn, request):
+    conn.write(StatusLine.OK_200)
+    conn.write(ResponseHeader.CONNECTION_CLOSE)
+    conn.write(CRLF)
+    raise Exception("Stop Server")
 
-            path = request["path"]
 
-            if path == "/":
-                http.sendfile(conn, "index.html")
+@app.route("GET", "/api/reset")
+def stop(conn, request):
+    conn.write(StatusLine.OK_200)
+    conn.write(ResponseHeader.CONNECTION_CLOSE)
+    conn.write(CRLF)
+    time.sleep_ms(500)
+    machine.reset()
 
-            if path == "/favicon.ico":
-                http.sendfile(conn, "favicon.ico")
 
-            if path == "/api/stop":  # stop server, for dev purposes
-                conn.write("\n")
-                conn.close()
-                break
-
-            if path == "/api/reset":
-                conn.write("\n")
-                conn.close()
-                reset()
-
-            if path == "/api/init":
-                settings = dict()
-                with lock:
-                    settings["temp0"] = max(temp0, temp1)
-                    settings["temp1"] = min(temp0, temp1)
-                settings["dutycycle"] = dutycycle
-                settings["end_dutycycle"] = end_dutycycle
-                settings["start_dutycycle"] = start_dutycycle
-                settings["temp_fan_on"] = temp_fan_on
-                settings["temp_fan_end"] = temp_fan_end
-                settings["hysteresis"] = hysteresis
-                conn.write(json.dumps(settings))
-
-            if path == "/api/set":
-                parameters = request["parameters"]
-                if "end_dutycycle" in parameters:
-                    end_dutycycle = int(parameters["end_dutycycle"])
-                    pycom.nvs_set("end_dutycycle", end_dutycycle)
-                if "start_dutycycle" in parameters:
-                    start_dutycycle = int(parameters["start_dutycycle"])
-                    pycom.nvs_set("start_dutycycle", start_dutycycle)
-                if "temp_fan_on" in parameters:
-                    temp_fan_on = int(parameters["temp_fan_on"])
-                    pycom.nvs_set("temp_fan_on", temp_fan_on)
-                if "temp_fan_end" in parameters:
-                    temp_fan_end = int(parameters["temp_fan_end"])
-                    pycom.nvs_set("temp_fan_end", temp_fan_end)
-                if "hysteresis" in parameters:
-                    hysteresis = int(parameters["hysteresis"])
-                    pycom.nvs_set("hysteresis", hysteresis)
-                conn.write(json.dumps(parameters))
-
-            if path == "/api/status":
-                values = dict()
-                with lock:
-                    values["temp0"] = max(temp0, temp1)
-                    values["temp1"] = min(temp0, temp1)
-                values["dutycycle"] = dutycycle
-                conn.write(json.dumps(values))
-
-            conn.write("\n")
-            conn.close()
-    except Exception as e:
-        serversocket.close()
-        raise e
-    finally:
-        serversocket.close()
+app.start()  # does not return unless stopped by Exception("Stop Server")
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-import controller
 import pycom
-
+import time
+import sys
+import gc
 
 pycom.heartbeat(False)
 
-while True:
-    try:
-        print("starting server")
-        pycom.rgbled(0x001000)  # green led if server is working
-        controller.server()
-        pycom.heartbeat(True)  # flashing blue led if server was stopped normally
-        break
-    except Exception as e:
-        print("server exception", repr(e))
-        pycom.rgbled(0x100000)  # red led if an error occured
-        import sys
-        sys.print_exception(e)
-        break  # dev only, to be replaced by sleep() and machine.reset()
+try:
+    pycom.rgbled(0x001000)  # green led if server is working
+    import controller
+
+    pycom.heartbeat(True)  # flashing blue led if server was stopped normally
+except Exception as e:
+    pycom.rgbled(0x100000)  # red led if an error occured
+
+    # write exception info to file for debugging purposes
+    t = time.localtime()
+    filename = "{:04}{:02}{:02}-{:02}{:02}{:02}.txt".format(t[0], t[1], t[2], t[3], t[4], t[5])
+    with open(filename, 'w') as output:
+        output.write("exception: {}\n".format(repr(e)))
+        sys.print_exception(e, output)
+        output.write("memory: {} bytes free\n".format(gc.mem_free()))  # WiPy 2 is memory constrained
